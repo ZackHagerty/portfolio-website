82ca183b9532ce37320cba7a56e4583eeff2b073
Zack Hagerty <70977089+ZackHagerty@users.noreply.github.com>
2024-10-31 00:22:15 -0400

    Setup placeholder for rest of home page
    note to self: I'm thinking a separate page for experience might be valuable

--- portfolio-website/src/pages/homePage/homePage.jsx
+++ portfolio-website/src/pages/homePage/homePage.jsx
@@ -16,8 +16,69 @@ export default function HomePage()
                 <p className="header__description">Lorem ipsum dolor sit amet consectetur adipisicing elit.  Libero illo maiores incidunt!</p>
             </div>
         </section>
-        <section className='body'>
-            
+        <section className='experience'>
+            <div clasName='experience__header'>
+                <h3 className='experience__title'>Experience</h3>
+                <hr className='experience__divider'/>
+            </div>
+            <div className='experience-block__head'>
+                <div className="circle"></div>
+                <div className='experience-block__job'>
+                    <h3 className="experience-block__sub-title">Software Engineer - Flexjet</h3>
+                    <p className="experience-block__description">Description</p>
+                </div>
+            </div>
+            <div className='experience__block--body'></div>            
+            <div className='experience__block--tail'></div>            
         </section>
+        <section className='experience'>
+            <div clasName='experience__header'>
+                <h3 className='experience__title'>Patents</h3>
+                <hr className='experience__divider'/>
+            </div>
+            <div className="patents-body">
+            <div className="patent-container">
+                <h4>Placeholder</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit nisi, dignissimos cumque voluptatibus aliquam nam labore totam dolorem voluptate quibusdam ipsa qui nulla mollitia sunt rem sequi. Fugit, amet repudiandae!</p>
+            </div> 
+            <div className="patent-container">
+                <h4>Placeholder</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit nisi, dignissimos cumque voluptatibus aliquam nam labore totam dolorem voluptate quibusdam ipsa qui nulla mollitia sunt rem sequi. Fugit, amet repudiandae!</p>
+            </div>   
+            </div>         
+        </section>
+        <section className='experience'>
+            <div clasName='experience__header'>
+                <h3 className='experience__title'>Projects</h3>
+                <hr className='experience__divider'/>
+            </div>
+            <div className="patents-body">
+            <div className="patent-container">
+                <h4>Placeholder</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit nisi, dignissimos cumque voluptatibus aliquam nam labore totam dolorem voluptate quibusdam ipsa qui nulla mollitia sunt rem sequi. Fugit, amet repudiandae!</p>
+            </div> 
+            <div className="patent-container">
+                <h4>Placeholder</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit nisi, dignissimos cumque voluptatibus aliquam nam labore totam dolorem voluptate quibusdam ipsa qui nulla mollitia sunt rem sequi. Fugit, amet repudiandae!</p>
+            </div>   
+            </div>         
+        </section>
+        <section className='experience'>
+            <div clasName='experience__header'>
+                <h3 className='experience__title'>Education</h3>
+                <hr className='experience__divider'/>
+            </div>
+            <div className="patents-body">
+            <div className="patent-container">
+                <h4>Placeholder</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit nisi, dignissimos cumque voluptatibus aliquam nam labore totam dolorem voluptate quibusdam ipsa qui nulla mollitia sunt rem sequi. Fugit, amet repudiandae!</p>
+            </div> 
+            <div className="patent-container">
+                <h4>Placeholder</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit nisi, dignissimos cumque voluptatibus aliquam nam labore totam dolorem voluptate quibusdam ipsa qui nulla mollitia sunt rem sequi. Fugit, amet repudiandae!</p>
+            </div>   
+            </div>         
+        </section>
+        
     </>
 }
