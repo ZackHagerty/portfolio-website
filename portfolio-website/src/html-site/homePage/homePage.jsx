6a576173da6111a9d67b6db7d313e097a9a21741
Zack Hagerty <70977089+ZackHagerty@users.noreply.github.com>
2025-02-02 23:53:21 -0500

    Changed the font
    Changed some design structure more.

--- portfolio-website/src/html-site/homePage/homePage.jsx
+++ portfolio-website/src/html-site/homePage/homePage.jsx
@@ -1,8 +1,8 @@
 import Scene from '../../three.js/scene'
 import Navbar from '../../utility/navbar/navbar'
 import Experience from './experience/experience'
-
 import './homePage.scss'
+import Patents from './patents/patents'
 
 
 export default function HomePage()
@@ -12,38 +12,8 @@ export default function HomePage()
             {/* <Navbar />  */}
             <Scene/>
         </section>
-            <Experience/>
-        <section className='patent'>
-            <div className='experience__header'>
-                <h3 className='experience__title'>Patents</h3>
-                <hr className='experience__divider'/>
-            </div>
-            <div className="patents-body">
-            <div className="patent-container">
-                <h4>Fleet Optimizer</h4>
-                <hr className='experience__divider utility__spacer'/>
-                <p>
-                   This patent outlines a project that dynamically allocates aircraft to establish an acceptable flight schedule, 
-                   taking into account requests and the availability of associated crews. The computer can repeatedly run the algorithm as 
-                   scheduling requests are updated.
-                </p>
-                <div className='patent-footer'>
-                    <button className='patent__button'>Read more</button>
-                </div>
-            </div> 
-            <div className="patent-container">
-            <h4>Demand Forecasting For Transportation Services</h4>
-            <hr className='experience__divider utility__spacer'/>
-                <p>
-                    This patent outlines a project that utilizes machine-learning techniques to predict the demand of aircraft, by type, 
-                    for a company.
-                </p>
-                <div className='patent-footer'>
-                    <button className='patent__button'>Read more</button>
-                </div>
-            </div>   
-            </div>         
-        </section>
+        <Experience/>
+        <Patents />
         <section className='experience'>
             <div className='experience__header'>
                 <h3 className='experience__title'>Projects</h3>
